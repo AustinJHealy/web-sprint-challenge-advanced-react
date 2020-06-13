126e5b5180acf19ef090594e81ad9e4b54bbebaa
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-06-12 20:37:30 -0400

    2 tests passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,17 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
-    const { getByText } = render(<CheckoutForm />)
-test("form shows success message on submit with form details", () => {});
+test("form header renders", () => {  const { getByText } = render(<CheckoutForm />)
+     const headerForm = getByText(/checkout form/i)
+    expect(headerForm).toBeInTheDocument()
+    });
+  
+test("form shows success message on submit with form details", () => {
+    const { getByTestId } = render (<CheckoutForm />)
+
+     const checkoutButton = getByTestId('submit')
+     fireEvent.click(checkoutButton)
+
+     const successMessage = getByTestId('successMessage')
+     expect(successMessage).toBeInTheDocument()
+});
